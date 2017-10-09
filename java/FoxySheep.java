45cd8a83bcd27c9f975f8adf5d2fce279d231d35
Robert Teed <robert@emeraldtherapeutics.com>
2017-10-09 11:55:49 -0700

    FoxySheep.java: a misc collection of changes:
    - define getErrors function and AtomError class to fetch errors (for linter)
    - fetch tokens on-the-fly from model using getAtomTokensForRange
    - parse has a start offset
    - tokensMap_ is useless. I should not have commited its addition but here we are...

--- java/FoxySheep.java
+++ java/FoxySheep.java
@@ -16,6 +16,7 @@ import com.wolfram.mexpr.MSymbol;
 import com.wolfram.mexpr.IMExprToken;
 import java.io.ByteArrayOutputStream;
 import java.util.Map;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.HashMap;
 import java.util.List;
@@ -29,6 +30,7 @@ public class FoxySheep {
 	public MathematicaSourceModel model;
 	VariableLocator varLocator_ = null;
 	DocumentBuffer sourceBuffer_ = null;
+	HashMap tokensMap_;
 	HashMap localVariablesMap_;
 	List changedLocalVariablePositions_ = new ArrayList();
 	
@@ -57,7 +59,7 @@ public class FoxySheep {
 		
 		buildModelFromText(str);
 		
-		parse();
+		parse(0);
 		
 	}
 	
@@ -71,16 +73,130 @@ public class FoxySheep {
 	
 	public void parseText(String text) throws Exception {
 		buildModelFromText(text);
-		parse();
+		parse(0);
 	}
 	
-	public void parse() throws Exception {
+	public void parse(int start) throws Exception {
 		
 		tokens_ = new ArrayList<AtomToken>();
+		tokensMap_ = new HashMap();
 		
 		System.out.println("TOKS:");
-		int i=0;
+		int i=start;
 		while (true) {
+			IMExprToken tok = null;
+			try {
+				tok = model.getToken(i);
+			}
+			catch (Exception ex) {
+				break;
+			}
+			if (tok == null) {
+				break;
+			}
+			if (tok.getText() == null || ((tok.getCharEnd() - tok.getCharStart() + 1) != tok.getText().length())) {
+				//tokens_.add(null);
+				i++;
+				continue;
+			}
+			VariableData varResult = (VariableData)localVariablesMap_.get(tok.getCharStart());
+
+			if (varResult == null) {
+				AtomToken atomTok = new AtomToken(tok, false, i);
+				tokens_.add(atomTok);
+				tokensMap_.put(i,atomTok);
+			} else {
+				AtomToken atomTok = new AtomToken(tok, true, i);
+				tokens_.add(atomTok);
+				tokensMap_.put(i,atomTok);
+			}
+			i++;
+		}
+	}
+	
+	public int getFixedStartTokenIndex(int initialIndex) {
+
+		IMExprToken tok = model.getToken(initialIndex);
+		
+		if (!(tok.getText() == null || ((tok.getCharEnd() - tok.getCharStart() + 1) != tok.getText().length()))) {
+			return initialIndex;
+		}
+		
+		int fixedIndex = initialIndex + 1;
+		return getFixedStartTokenIndex(fixedIndex);
+	} 
+	
+	public int getFixedEndTokenIndex(int initialIndex) {
+		
+		IMExprToken tok;
+		try {
+			tok = model.getToken(initialIndex);
+		}
+		catch (Exception ex) {
+			return initialIndex - 1;
+		}
+		
+		if (tok == null) {
+			return initialIndex - 1;
+		}
+				
+		if (!(tok.getText() == null || ((tok.getCharEnd() - tok.getCharStart() + 1) != tok.getText().length()))) {
+			return initialIndex - 1;
+		}
+		
+		int fixedIndex = initialIndex + 1;
+		return getFixedEndTokenIndex(fixedIndex);
+	} 
+	
+	public AtomToken[] getAtomTokensForRange(int startOffset, int endOffset) {
+		
+		int startIndex = model.getTokenIndexForOffset(startOffset);
+		int endIndex = model.getTokenIndexForOffset(endOffset);
+		
+		int fixedStartIndex = getFixedStartTokenIndex(startIndex);
+		int fixedEndIndex = getFixedEndTokenIndex(endIndex);
+		
+		List<AtomToken> newTokens = new ArrayList<AtomToken>();
+		
+		for (int i = fixedStartIndex; i <= fixedEndIndex ; i++) {
+			IMExprToken tok = null;
+			try {
+				tok = model.getToken(i);
+			}
+			catch (Exception ex) {
+				break;
+			}
+			if (tok == null) {
+				continue;
+			}
+			if (tok.getText() == null || ((tok.getCharEnd() - tok.getCharStart() + 1) != tok.getText().length())) {
+				continue;
+			}
+			VariableData varResult = (VariableData)localVariablesMap_.get(tok.getCharStart());
+
+			if (varResult == null) {
+				AtomToken atomTok = new AtomToken(tok, false, i);
+				newTokens.add(atomTok);
+			} else {
+				AtomToken atomTok = new AtomToken(tok, true, i);
+				newTokens.add(atomTok);
+			}
+		}
+		
+		return newTokens.toArray(new AtomToken[newTokens.size()]);
+	}
+	
+	public void parseIncr(int offset, int length, int newLength, SourceBufferChangeEvent event) throws Exception {
+		int startIndex = getTokenIndexForOffset(offset);
+		int endIndex = getTokenIndexForOffset(offset+length);
+		//int endIndex = tokens_.size() - 1;
+		
+		int modelStartIndex = model.getTokenIndexForOffset(offset);
+		int modelEndIndex = model.getTokenIndexForOffset(offset+newLength);
+		
+		List<AtomToken> newTokens = new ArrayList<AtomToken>();
+		
+		for (int i = modelStartIndex; i <= modelEndIndex; i++) {
 			IMExprToken tok = null;
 			try {
 				tok = model.getToken(i);
@@ -96,14 +212,22 @@ public class FoxySheep {
 				continue;
 			}
 			VariableData varResult = (VariableData)localVariablesMap_.get(tok.getCharStart());
-
+	
 			if (varResult == null) {
-				tokens_.add(new AtomToken(tok, false));
+				newTokens.add(new AtomToken(tok, false, i));
 			} else {
-				tokens_.add(new AtomToken(tok, true));
+				newTokens.add(new AtomToken(tok, true, i));
 			}
-			i++;
 		}
+		
+		int fixIndex = newTokens.size() - (endIndex - startIndex + 1);
+		int[] vals = getTextFix(event);
+		
+		fixTokenPositions(endIndex, vals[2], fixIndex);
+		
+		tokens_.subList(startIndex, endIndex).clear();
+		tokens_.addAll(startIndex, newTokens);
+		
 	}
 	
 	public int[] bufferChanged(String text, int offset, int length, String newText, Boolean parseVars) throws Exception {
@@ -118,7 +242,7 @@ public class FoxySheep {
 		
 		if (!parseVars) {
 			sourceBuffer_.fireChangeEvent(changeEvent);
-			parse();
+			parse(0);
 			span[0] = minOffset;
 			span[1] = maxLength;
 			return span;
@@ -130,7 +254,7 @@ public class FoxySheep {
 		ModelRegion updatedRegion;
 		
 		if (!parseVars) {
-			parse();
+			parse(0);
 			span[0] = minOffset;
 			span[1] = maxLength;
 			return span;
@@ -144,7 +268,7 @@ public class FoxySheep {
 		}
 		getLocalVariablesMap();
 		
-		parse();
+		parse(0);
 		
 		if (updatedRegion == null) {
 			span[0] = -1;
@@ -158,6 +282,73 @@ public class FoxySheep {
 		return span;
 	}
 	
+	private int[] getTextFix(SourceBufferChangeEvent event) {
+		int start = event.getOffset();
+		int end = start + event.getLength();
+		int fixLen = event.getTextLength() - event.getLength();
+		return new int[] { start, end, fixLen };
+	}
+	
+	private void fixTokenPositions(int start, int fixLen, int fixIndex) {
+		for (int i = start; i < tokens_.size(); i++) {
+			AtomToken token = (AtomToken)tokens_.get(i);
+			IMExprToken tok = token.tok;
+			tok.fixTokenPosition(fixLen, fixIndex);
+			token.start = (tok.getCharStart());
+			token.end = (tok.getCharEnd());
+		}
+		
+	}
+	
+	public void tokenizeChange(String text, int offset, int length, String newText, Boolean parseVars) throws Exception {
+		sourceBuffer_.set(text);
+		
+		SourceBufferChangeEvent changeEvent = new BufferChangeEvent(sourceBuffer_,offset,length,newText);
+		
+		int minOffset = changeEvent.getMinOffset();
+		int maxLength = changeEvent.getMaxLength();
+
+		model.addDocumentEvent(changeEvent);
+		sourceBuffer_.fireChangeEvent(changeEvent);
+		
+		return;
+	}
+	
+	public void parseChanges() throws Exception {
+		getLocalVariablesMap();
+		parse(0);
+		return;
+	}
+	
+	public int[] parseChange(int offset, int length, String newText) throws Exception {
+		
+		SourceBufferChangeEvent changeEvent = new BufferChangeEvent(sourceBuffer_,offset,length,newText);
+		
+		int[] span = new int[2];
+		
+		int minOffset = changeEvent.getMinOffset();
+		int maxLength = changeEvent.getMaxLength();
+		
+		ModelRegion updatedRegion;
+		try {
+			updatedRegion = model.checkVars(minOffset,maxLength);
+		} catch (Exception ex) {
+			updatedRegion = null;
+			System.out.println(ex);
+		}
+		Expression expression = model.getRootExpressionForOffset(offset);
+		if (expression == null) {
+			span[0] = -1;
+			span[1] = 0;
+		} else {
+			span = expression.getSourceRange();
+		}
+		
+		getLocalVariablesMap();
+
+		return span;
+	}
+	
 	public void bufferDoneChanging(int offset, int length) throws Exception {
 		try {
 			model.checkVars(offset,length);
@@ -165,7 +356,7 @@ public class FoxySheep {
 			System.out.println(ex);
 		}
 		
-		parse();
+		parse(0);
 	}
 	
 	public class BufferChangeEvent implements SourceBufferChangeEvent {
@@ -313,16 +504,45 @@ public class FoxySheep {
 		// * `length` The length of the text within the token
 		public String text;
 		public int scopeInt;
+		public int index;
 		public Boolean scopedVar;
 		public int start;
 		public int end;
 		public int len;
 		public IMExprToken tok;
 		
-		public AtomToken(IMExprToken token, Boolean scopedVariable) {
+		public AtomToken(MExpr expr, Boolean scopedVariable) {
+			
+			if (expr.sameQ(MExpr.NULLexpr)) {
+				return;
+			}
+			
+			start = model.startPositionFromTokenIndex(expr);
+			end = model.endPositionFromTokenIndex(expr);
+			
+			if (scopedVariable) {
+				scopedVar = true;
+			} else {
+				scopedVar = false;
+			}
+			
+			text = expr.toString();
+			
+			if (expr.sameQ(MExpr.STRINGexpr)) {
+				scopeInt = 99;
+			} else {
+				scopeInt = 86;
+			}
+			
+			len = end-start;
+			
+		}
+		
+		public AtomToken(IMExprToken token, Boolean scopedVariable, int ind) {
 			tok = token;
 			start = (token.getCharStart());
 			text = token.getText();
+			index = ind;
 			
 			if (text != null) {
 				len = text.length();
@@ -330,8 +550,8 @@ public class FoxySheep {
 				len = 0;
 			}
 			
-			//end = (start + len);
-			end = (token.getCharEnd());
+			end = (start + len);
+			//end = (token.getCharEnd());
 			System.out.println("ihihihi");
 			System.out.println(end);
 			
@@ -342,6 +562,23 @@ public class FoxySheep {
 			} else {
 				scopedVar = false;
 			}
+		}
+	}
+	
+	public class AtomError {
+		public String description;
+		public String longText;
+		public int start;
+		public int end;
+		public Boolean warning;
+		
+		public AtomError(SyntaxError error) {
+			
+			description = error.getDescription();
+			longText = error.getLongText();
+			start = error.getCharStart();
+			end = error.getCharEnd();
+			warning = error.isWarning();
 		}
 	}
 	
@@ -369,6 +606,21 @@ public class FoxySheep {
 		
 		return localVariablesMap_;
 		
+	}
+	
+	public AtomError[] getErrors() {
+		
+		Collection collection = model.getErrors();
+		
+		AtomError[] atomErrors = new AtomError[collection.size()];
+		
+		for (int i = 0; i < collection.size(); i++) {
+			SyntaxError err = (SyntaxError)collection.toArray()[i];
+		  AtomError atomError = new AtomError(err);
+			atomErrors[i] = atomError;
+		}
+		
+		return atomErrors;
 	}
 	
 	public void fixLocalVariablePositions() {
@@ -414,11 +666,13 @@ public class FoxySheep {
 		int max = size;
 		int pos = max / 2;
 		
-
-		if (offset <= ((AtomToken)tokens_.get(0)).start) {
+		AtomToken firstToken = ((AtomToken)tokens_.get(0));
+		AtomToken finalToken = ((AtomToken)tokens_.get(size));
+		
+		if (firstToken != null && offset <= firstToken.start) {
 			return 0;
 		}
-		if (offset >= ((AtomToken)tokens_.get(size)).start) {
+		if (finalToken!=null && offset >= finalToken.start) {
 			return size;
 		}
 		
@@ -428,9 +682,11 @@ public class FoxySheep {
 				if (offset <= tok.end) {
 					return pos;
 				}
-				if ((pos < size) && 
-					(offset < ((AtomToken)tokens_.get(pos + 1)).start)) {
-					return pos + 1;
+				if (pos < size) {
+					AtomToken nextToken = ((AtomToken)tokens_.get(pos + 1));
+					if (nextToken != null && offset < nextToken.start) {
+						return pos + 1;
+					}
 				}
 				int oldpos = pos;
 				pos = (max + pos) / 2;
@@ -439,9 +695,63 @@ public class FoxySheep {
 				}
 				min = oldpos;
 			} else {
-				if ((pos > 0) && 
-					(((AtomToken)tokens_.get(pos - 1)).end < offset)) {
-					return pos;
+				if (pos > 0) {
+					AtomToken lastToken = ((AtomToken)tokens_.get(pos - 1));
+					if (lastToken != null && lastToken.end < offset) {
+						return pos;
+					}
+				}
+				
+				int oldpos = pos;
+				pos = (pos + min) / 2;
+				max = oldpos;
+			}
+		}
+		
+		return -1;
+	}
+	
+	public int getModelTokenIndexForOffset(int offset)
+	{
+		int size = tokens_.size() - 1;
+		int min = 0;
+		int max = size;
+		int pos = max / 2;
+		
+		AtomToken firstToken = ((AtomToken)tokens_.get(0));
+		AtomToken finalToken = ((AtomToken)tokens_.get(size));
+		
+		if (firstToken != null && offset <= firstToken.start) {
+			return 0;
+		}
+		if (finalToken!=null && offset >= finalToken.start) {
+			return size;
+		}
+		
+		while (min < max) {
+			AtomToken tok = (AtomToken)tokens_.get(pos);
+			if (tok.start <= offset) {
+				if (offset <= tok.end) {
+					return tok.index;
+				}
+				if (pos < size) {
+					AtomToken nextToken = ((AtomToken)tokens_.get(pos + 1));
+					if (nextToken != null && offset < nextToken.start) {
+						return nextToken.index + 1;
+					}
+				}
+				int oldpos = pos;
+				pos = (max + pos) / 2;
+				if (pos == oldpos) {
+					pos = max;
+				}
+				min = oldpos;
+			} else {
+				if (pos > 0) {
+					AtomToken lastToken = ((AtomToken)tokens_.get(pos - 1));
+					if (lastToken != null && lastToken.end < offset) {
+						return tok.index;
+					}
 				}
 				
 				int oldpos = pos;
